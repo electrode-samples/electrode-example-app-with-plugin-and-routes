f1ce28136dcb8022b1787f64f02edaa239ed74f1
James McIntyre <jmcintyre@walmartlabs.com>
2016-09-20 18:36:49 -0700

    made test work for Home app component

--- test/client/components/home.spec.jsx
+++ test/client/components/home.spec.jsx
@@ -1,25 +1,33 @@
+require("isomorphic-fetch");
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { Home } from "client/components/home";
 
+class ChildComponent extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
 describe("Home", () => {
   let component;
   let container;
+  let location;
 
   beforeEach(() => {
     container = document.createElement("div");
-  });
-
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(container);
+    location = { pathname: "/" };
   });
 
   it("has expected content with deep render", () => {
     component = ReactDOM.render(
-      <Home/>,
-      container
-    );
-
+      <Home location={location}>
+        <ChildComponent />
+      </Home>,
+      container);
     expect(component).to.not.be.false;
   });
 });
